bdd7655b52c5d9190856a41bc2b5570a0873fb15
marwenshili <eshilimarwen9@gmail.com>
2023-12-21 17:04:33 +0100

    fx crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
+import * as dayjs from 'dayjs';
+import { uid } from 'uid-ts';
 
+interface ITask {
+  id: number;
+  title: string;
+  isDone: boolean;
+  createdAt: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-todo';
+  tasks: ITask[] = [];
+  DateNow = dayjs(Date.now()).format('DD/MM/YYYY');
+  isEditing: boolean = false;
+  taskToEdit: ITask | null = null;
+  indexTaskToEdit: number | null = null;
+
+  constructor() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+
+  generateTaskId = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  createOrUpdateTask = (newTask: ITask) => {
+    if (newTask.title.length === 0) return;
+    if (
+      this.isEditing &&
+      this.taskToEdit &&
+      typeof this.indexTaskToEdit === 'number'
+    ) {
+      this.updateTask(newTask);
+    } else {
+      this.createTask(newTask);
+    }
+  };
+
+  private createTask = (newTask: ITask) => {
+    this.tasks = [...this.tasks, newTask];
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  };
+
+  private updateTask = (updatedTask: ITask) => {
+    if (typeof this.indexTaskToEdit === 'number') {
+      this.tasks[this.indexTaskToEdit] = updatedTask;
+      this.isEditing = false;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  };
+  findTaskIndex = (id: number) => {
+    return this.tasks.findIndex((task) => task.id === id);
+  };
+
+  editTask = (id: number) => {
+    this.isEditing = true;
+    let index = this.findTaskIndex(id);
+    this.taskToEdit = this.tasks[index];
+    this.indexTaskToEdit = index;
+  };
+  deleteTask = (id: number) => {
+    let newTasks = this.tasks.filter((task) => task.id !== id);
+    this.tasks = [...newTasks];
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  };
+
+  markTaskAsDone = (id: number) => {
+    const index = this.findTaskIndex(id);
+    this.tasks[index].isDone = !this.tasks[index].isDone;
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  };
+
+  searchTask = (str?: string) => {
+    if (!str) {
+      return this.tasks;
+    }
+    let newTasks = this.tasks.filter((task) => task.title.includes(str));
+    return newTasks;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
